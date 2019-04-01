15a3d0ad65cdac9b351e8330cd00ede081b54764
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-04-01 22:26:04 +0200

    commented out parts of the network to make it simpler

--- unet.py
+++ unet.py
@@ -7,23 +7,23 @@ from keras.optimizers import Adam
 def unet(pretrained_weights=None, input_size=(256,256,1)):
     inputs = Input(input_size)
     conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
-    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
+    # conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
     conv5 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
-    conv5 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
+    # conv5 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     drop5 = Dropout(0.5)(conv5)
 
-    up6 = Conv2D(128, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2,2))(drop5))
+    up6 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([conv1,up6], axis=3)
-    conv6 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(3, 3, activation='sigmoid', padding='same', kernel_initializer='he_normal')(conv6)
+    # conv6 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(3, 3, activation='sigmoid', padding='same', kernel_initializer='he_normal')(merge6)
 
 
 
     model = Model(input=inputs, output=conv6)
 
-    model.compile(optimizer=Adam(lr=1e-4), loss='mean_squared_error', metrics=['mse'])
+    model.compile(optimizer=Adam(lr=1e-4), loss='mean_squared_error')
 
     #model.summary()
 
