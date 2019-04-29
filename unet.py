1ba6f041411493700349449c2a5b3bc987118645
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-04-29 11:04:54 +0200

    switched to single kernel for convolution

--- unet.py
+++ unet.py
@@ -4,20 +4,20 @@ from keras.layers import Conv2D, MaxPooling2D, concatenate, Dropout, UpSampling2
 from keras.optimizers import Adam
 
 
-def unet(pretrained_weights=None, input_size=(256,256,1)):
+def unet(pretrained_weights=None, input_size=(256,256,1), dropout=0.5):
     inputs = Input(input_size)
-    conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     # conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
 
-    conv5 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv5 = Conv2D(1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
     # conv5 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
-    drop5 = Dropout(0.5)(conv5)
+    drop5 = Dropout(dropout)(conv5)
 
-    up6 = Conv2D(64, 2, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    up6 = Conv2D(1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([conv1,up6], axis=3)
     # conv6 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(3, 3, activation='sigmoid', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(input_size[-1], 3, activation='sigmoid', padding='same', kernel_initializer='he_normal')(merge6)
 
 
 
@@ -28,6 +28,6 @@ def unet(pretrained_weights=None, input_size=(256,256,1)):
     #model.summary()
 
     if(pretrained_weights):
-    	model.load_weights(pretrained_weights)
+        model.load_weights(pretrained_weights)
 
     return model
