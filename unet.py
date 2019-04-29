eaeac67c59f6c7c97b864c2436ab61f5b421d676
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-04-29 20:20:14 +0200

    added possibility to have a different kernel size more easily

--- unet.py
+++ unet.py
@@ -1,23 +1,24 @@
 """Largely inspired by https://github.com/zhixuhao/unet/blob/master/model.py"""
 from keras.models import Model
-from keras.layers import Conv2D, MaxPooling2D, concatenate, Dropout, UpSampling2D, Input
+from keras.layers import Conv2D, MaxPooling2D, concatenate, Dropout, UpSampling2D, Input, AveragePooling2D
 from keras.optimizers import Adam
 
 
-def unet(pretrained_weights=None, input_size=(256,256,1), dropout=0.5):
+def unet(pretrained_weights=None, input_size=(256,256,1), dropout=0.5, kernel_size=3):
     inputs = Input(input_size)
-    conv1 = Conv2D(1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
+    conv1 = Conv2D(1, kernel_size , activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     # conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    # pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    pool1 = AveragePooling2D(pool_size=(2, 2))(conv1)
 
-    conv5 = Conv2D(1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
+    conv5 = Conv2D(1, kernel_size , activation='relu', padding='same', kernel_initializer='he_normal')(pool1)
     # conv5 = Conv2D(128, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv5)
     drop5 = Dropout(dropout)(conv5)
 
-    up6 = Conv2D(1, 3, activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
+    up6 = Conv2D(1, kernel_size , activation='relu', padding='same', kernel_initializer='he_normal')(UpSampling2D(size=(2, 2))(drop5))
     merge6 = concatenate([conv1,up6], axis=3)
     # conv6 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(merge6)
-    conv6 = Conv2D(input_size[-1], 3, activation='sigmoid', padding='same', kernel_initializer='he_normal')(merge6)
+    conv6 = Conv2D(input_size[-1], kernel_size , activation='sigmoid', padding='same', kernel_initializer='he_normal')(merge6)
 
 
 
