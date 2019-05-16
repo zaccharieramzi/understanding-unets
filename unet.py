4f0d5a8b0d8073ecfa549c20bd51980761043c1e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-05-16 15:14:47 +0200

    better factorization for the flexible unet

--- unet.py
+++ unet.py
@@ -1,11 +1,50 @@
 """Largely inspired by https://github.com/zhixuhao/unet/blob/master/model.py"""
-from keras.models import Model
+from keras import activations
+from keras import backend as K
 from keras.layers import Conv2D, MaxPooling2D, concatenate, Dropout, UpSampling2D, Input, AveragePooling2D
+from keras.models import Model
 from keras.optimizers import Adam
 
 
+def unet_rec(inputs, kernel_size=3, n_layers=1, layers_n_channels=1, layers_n_non_lins=1):
+    if n_layers == 1:
+        last_conv = chained_convolutions(
+            inputs,
+            n_channels=layers_n_channels[0],
+            n_non_lins=layers_n_non_lins[0],
+            kernel_size=kernel_size,
+        )
+        output = last_conv
+    else:
+        # TODO: refactor the following
+        n_non_lins = layers_n_non_lins[0]
+        n_channels = layers_n_channels[0]
+        left_u = chained_convolutions(
+            inputs,
+            n_channels=n_channels,
+            n_non_lins=n_non_lins,
+            kernel_size=kernel_size,
+        )
+        rec_input = AveragePooling2D(pool_size=(2, 2))(left_u)
+        rec_output = unet_rec(
+            inputs=rec_input,
+            kernel_size=kernel_size,
+            n_layers=n_layers-1,
+            layers_n_channels=layers_n_channels[1:],
+            layers_n_non_lins=layers_n_non_lins[1:],
+        )
+        merge = concatenate([left_u, UpSampling2D(size=(2, 2))(rec_output)], axis=3)
+        output = chained_convolutions(
+            merge,
+            n_channels=n_channels,
+            n_non_lins=n_non_lins,
+            kernel_size=kernel_size,
+        )
+    return output
+
+
 def unet(
-        last_layer=True,
+        with_extra_sigmoid=True,
         pretrained_weights=None,
         input_size=(256, 256, 1),
         kernel_size=3,
@@ -22,45 +61,26 @@ def unet(
     else:
         assert len(layers_n_non_lins) == n_layers
     inputs = Input(input_size)
-    if n_layers == 1:
-        last_conv = chained_convolutions(
-            inputs,
-            n_channels=layers_n_channels[0],
-            n_non_lins=layers_n_non_lins[0],
-            kernel_size=kernel_size,
-        )
-        model = Model(input=inputs, output=last_conv)
+    output = unet_rec(
+        inputs,
+        kernel_size=kernel_size,
+        n_layers=n_layers,
+        layers_n_channels=layers_n_channels,
+        layers_n_non_lins=layers_n_non_lins,
+    )
+    if with_extra_sigmoid:
+        new_output = Conv2D(
+            input_size[-1],
+            kernel_size,
+            activation='sigmoid',
+            padding='same',
+            kernel_initializer='he_normal',
+        )(output)
+        model = Model(inputs=inputs, outputs=new_output)
     else:
-        # TODO: refactor the following
-        n_non_lins = layers_n_non_lins[0]
-        n_channels = layers_n_channels[0]
-        left_u = chained_convolutions(
-            inputs,
-            n_channels=n_channels,
-            n_non_lins=n_non_lins,
-            kernel_size=kernel_size,
-        )
-        rec_input = AveragePooling2D(pool_size=(2, 2))(left_u)
-        rec_output = unet(
-            last_layer=False,
-            input_size=rec_input.shape,
-            kernel_size=kernel_size,
-            n_layers=n_layers-1,
-            layers_n_channels=layers_n_channels[1:],
-            layers_n_non_lins=layers_n_non_lins[1:],
-        )(rec_input)
-        merge = concatenate([left_u, UpSampling2D(size=(2, 2))(rec_output)], axis=3)
-        higher_scale_output = chained_convolutions(
-            merge,
-            n_channels=n_channels,
-            n_non_lins=n_non_lins,
-            kernel_size=kernel_size,
-        )
-        model = Model(inputs=inputs, outputs=higher_scale_output)
-
-
-    if last_layer:
-        model.compile(optimizer=Adam(lr=1e-4), loss='mean_squared_error')
+        model = Model(input=inputs, outputs=output)
+        model.layers[-1].activation = activations.sigmoid
+    model.compile(optimizer=Adam(lr=1e-4), loss='mean_squared_error')
 
     if pretrained_weights:
         model.load_weights(pretrained_weights)
