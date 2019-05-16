8eef93be18884db0227e9997b10ff80a27e1c3db
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-05-16 14:37:21 +0200

    created new flexible unet architecture

--- unet.py
+++ unet.py
@@ -4,7 +4,71 @@ from keras.layers import Conv2D, MaxPooling2D, concatenate, Dropout, UpSampling2
 from keras.optimizers import Adam
 
 
-def unet(pretrained_weights=None, input_size=(256,256,1), dropout=0.5, kernel_size=3):
+def unet(
+        last_layer=True,
+        pretrained_weights=None,
+        input_size=(256, 256, 1),
+        kernel_size=3,
+        n_layers=1,
+        layers_n_channels=1,
+        layers_n_non_lins=1,
+    ):
+    if isinstance(layers_n_channels, int):
+        layers_n_channels = [layers_n_channels] * n_layers
+    else:
+        assert len(layers_n_channels) == n_layers
+    if isinstance(layers_n_non_lins, int):
+        layers_n_non_lins = [layers_n_non_lins] * n_layers
+    else:
+        assert len(layers_n_non_lins) == n_layers
+    inputs = Input(input_size)
+    if n_layers == 1:
+        last_conv = chained_convolutions(
+            inputs,
+            n_channels=layers_n_channels[0],
+            n_non_lins=layers_n_non_lins[0],
+            kernel_size=kernel_size,
+        )
+        model = Model(input=inputs, output=last_conv)
+    else:
+        # TODO: refactor the following
+        n_non_lins = layers_n_non_lins[0]
+        n_channels = layers_n_channels[0]
+        left_u = chained_convolutions(
+            inputs,
+            n_channels=n_channels,
+            n_non_lins=n_non_lins,
+            kernel_size=kernel_size,
+        )
+        rec_input = AveragePooling2D(pool_size=(2, 2))(left_u)
+        rec_output = unet(
+            last_layer=False,
+            input_size=rec_input.shape,
+            kernel_size=kernel_size,
+            n_layers=n_layers-1,
+            layers_n_channels=layers_n_channels[1:],
+            layers_n_non_lins=layers_n_non_lins[1:],
+        )(rec_input)
+        merge = concatenate([left_u, UpSampling2D(size=(2, 2))(rec_output)], axis=3)
+        higher_scale_output = chained_convolutions(
+            merge,
+            n_channels=n_channels,
+            n_non_lins=n_non_lins,
+            kernel_size=kernel_size,
+        )
+        model = Model(inputs=inputs, outputs=higher_scale_output)
+
+
+    if last_layer:
+        model.compile(optimizer=Adam(lr=1e-4), loss='mean_squared_error')
+
+    if pretrained_weights:
+        model.load_weights(pretrained_weights)
+
+    return model
+
+
+def old_unet(pretrained_weights=None, input_size=(256, 256, 1), dropout=0.5, kernel_size=3):
     inputs = Input(input_size)
     conv1 = Conv2D(1, kernel_size , activation='relu', padding='same', kernel_initializer='he_normal')(inputs)
     # conv1 = Conv2D(64, 3, activation='relu', padding='same', kernel_initializer='he_normal')(conv1)
@@ -32,3 +96,16 @@ def unet(pretrained_weights=None, input_size=(256,256,1), dropout=0.5, kernel_si
         model.load_weights(pretrained_weights)
 
     return model
+
+
+def chained_convolutions(inputs, n_channels=1, n_non_lins=1, kernel_size=3):
+    conv = inputs
+    for _ in range(n_non_lins):
+        conv = Conv2D(
+            n_channels,
+            kernel_size,
+            activation='relu',
+            padding='same',
+            kernel_initializer='he_normal',
+        )(conv)
+    return conv
