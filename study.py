a66c3f34bbac2f519fcf0c2798ad4e1239388d80
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-05-23 17:10:05 +0200

    adapted python study file to be run in parallel in cluster

--- study.py
+++ study.py
@@ -1,11 +1,10 @@
 import os.path as op
 
-import numpy as np
+import click
 from keras import backend as K
 from keras.callbacks import TensorBoard
-from keras_tqdm import TQDMCallback
+import numpy as np
 from tensorflow import set_random_seed
-from tqdm import tqdm_notebook
 
 from data import im_generator
 from unet import unet
@@ -30,22 +29,24 @@ im_gen_val = im_generator(mode='validation', validation_split=0.1, batch_size=ba
 im_gen_test = im_generator(mode='testing', batch_size=batch_size, source=source, noise_std=noise_std)
 
 
-params = {
-    'cifar_classic_1': {'n_layers': 2},
-    'cifar_without_relu_contracting_1': {'n_layers': 2, 'non_relu_contract': True},
-    'cifar_aver_pool_1': {'n_layers': 2, 'pool': 'average'},
-    'cifar_classic_2': {'n_layers': 3},
-    'cifar_without_relu_contracting_2': {'n_layers': 3, 'non_relu_contract': True},
-    'cifar_aver_pool_2': {'n_layers': 3, 'pool': 'average'},
-    'cifar_classic_3': {'n_layers': 4},
-    'cifar_without_relu_contracting_3': {'n_layers': 4, 'non_relu_contract': True},
-    'cifar_aver_pool_3': {'n_layers': 4, 'pool': 'average'},
-}
+params = [
+    ('cifar_classic_1', {'n_layers': 2}),
+    ('cifar_without_relu_contracting_1', {'n_layers': 2, 'non_relu_contract': True}),
+    ('cifar_aver_pool_1', {'n_layers': 2, 'pool': 'average'}),
+    ('cifar_classic_2', {'n_layers': 3}),
+    ('cifar_without_relu_contracting_2', {'n_layers': 3, 'non_relu_contract': True}),
+    ('cifar_aver_pool_2', {'n_layers': 3, 'pool': 'average'}),
+    ('cifar_classic_3', {'n_layers': 4}),
+    ('cifar_without_relu_contracting_3', {'n_layers': 4, 'non_relu_contract': True}),
+    ('cifar_aver_pool_3', {'n_layers': 4, 'pool': 'average'}),
+]
 
-
-n_epochs = 1
-K.clear_session()
-for run_id, run_params in tqdm_notebook(params.items()):
+@click.command()
+@click.argument('params_id', help='The id of the params in the list of params {}.'.format(params))
+@click.option('--epochs', '-e', default=50, help='number of epochs to run.')
+def run_study(params_id, epochs):
+    K.clear_session()
+    run_id, run_params = params[params_id]
     print(run_id)
     model = unet(input_size=(size, size, 1), with_extra_sigmoid=False, **run_params)
     log_dir = op.join('logs', run_id)
@@ -59,10 +60,10 @@ for run_id, run_params in tqdm_notebook(params.items()):
     model.fit_generator(
         im_gen_train,
         steps_per_epoch=int((1-validation_split) * n_samples_train / batch_size),
-        epochs=n_epochs,
+        epochs=epochs,
         validation_data=im_gen_val,
         validation_steps=int(validation_split * n_samples_train / batch_size),
         verbose=0,
-        callbacks=[TQDMCallback(), tboard_cback],
+        callbacks=[tboard_cback],
     )
     K.clear_session()
