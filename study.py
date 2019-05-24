88aee533011a7ee9c2251bf07cd3ce9aa5bba53c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-05-24 16:39:06 +0200

    made study able to accept file params

--- study.py
+++ study.py
@@ -1,3 +1,4 @@
+import json
 import os.path as op
 import time
 
@@ -14,41 +15,55 @@ np.random.seed(1)
 set_random_seed(1)
 
 
-batch_size = 32
-noise_std = 30
-source = 'cifar_grey'
-validation_split = 0.1
-if 'cifar' in source:
-    n_samples_train = 5*1e4
-    size = 32
-elif 'mnist' in source:
-    n_samples_train = 6*1e4
-    size = 28
-n_samples_test = 1e4
-im_gen_train = im_generator(mode='training', validation_split=0.1, batch_size=batch_size, source=source, noise_std=noise_std)
-im_gen_val = im_generator(mode='validation', validation_split=0.1, batch_size=batch_size, source=source, noise_std=noise_std)
-im_gen_test = im_generator(mode='testing', batch_size=batch_size, source=source, noise_std=noise_std)
+def handle_source(source, batch_size=32, noise_std=30, validation_split=0.1):
+    if 'cifar' in source:
+        n_samples_train = 5*1e4
+        size = 32
+    elif 'mnist' in source:
+        n_samples_train = 6*1e4
+        size = 28
+    im_gen_train = im_generator(mode='training', validation_split=validation_split, batch_size=batch_size, source=source, noise_std=noise_std)
+    im_gen_val = im_generator(mode='validation', validation_split=validation_split, batch_size=batch_size, source=source, noise_std=noise_std)
+    im_gen_test = im_generator(mode='testing', batch_size=batch_size, source=source, noise_std=noise_std)
+    return im_gen_train, im_gen_val, im_gen_test, n_samples_train, size
 
 
-params = [
-    ('cifar_classic_1', {'n_layers': 2}),
-    ('cifar_without_relu_contracting_1', {'n_layers': 2, 'non_relu_contract': True}),
-    ('cifar_aver_pool_1', {'n_layers': 2, 'pool': 'average'}),
-    ('cifar_classic_2', {'n_layers': 3}),
-    ('cifar_without_relu_contracting_2', {'n_layers': 3, 'non_relu_contract': True}),
-    ('cifar_aver_pool_2', {'n_layers': 3, 'pool': 'average'}),
-    ('cifar_classic_3', {'n_layers': 4}),
-    ('cifar_without_relu_contracting_3', {'n_layers': 4, 'non_relu_contract': True}),
-    ('cifar_aver_pool_3', {'n_layers': 4, 'pool': 'average'}),
+def handle_params_file(params_file_path, source):
+    with open(params_file_path) as params_file:
+        params = json.load(params_file)
+    for param in params:
+        param[0] = '{source}_{param_desc}'.format(source=source, param_desc=param[0])
+    return params
+
+
+default_params = [
+    ('classic_1', {'n_layers': 2}),
+    ('without_relu_contracting_1', {'n_layers': 2, 'non_relu_contract': True}),
+    ('aver_pool_1', {'n_layers': 2, 'pool': 'average'}),
+    ('classic_2', {'n_layers': 3}),
+    ('without_relu_contracting_2', {'n_layers': 3, 'non_relu_contract': True}),
+    ('aver_pool_2', {'n_layers': 3, 'pool': 'average'}),
+    ('classic_3', {'n_layers': 4}),
+    ('without_relu_contracting_3', {'n_layers': 4, 'non_relu_contract': True}),
+    ('aver_pool_3', {'n_layers': 4, 'pool': 'average'}),
 ]
 
 @click.command()
-@click.option('--params-id', '-p', help='The id of the params in the list of params {}.'.format(params), default=0)
+@click.option('--params-id', '-p', help='The id of the params in the list of params.', default=0)
+@click.option('--params-file', '-f', help='File containing the list of params in a json format.', default=None, type=click.Path())
+@click.option('--source', '-s', help='Dataset on which to run the experiment.', type=click.Choice(['cifar_grey', 'cifar', 'mnist']), default='mnist')
 @click.option('--epochs', '-e', default=50, help='number of epochs to run.')
-def run_study(params_id, epochs):
+def run_study(params_id, params_file, source, epochs):
+    validation_split = 0.1
+    batch_size = 32
     start = time.time()
     K.clear_session()
+    if params_file is None:
+        params = default_params
+    else:
+        params = handle_params_file(params_file, source)
     run_id, run_params = params[params_id]
+    im_gen_train, im_gen_val, _, n_samples_train, size = handle_source(source, batch_size=batch_size, noise_std=30, validation_split=validation_split)
     print('Running {run_id}, for {epochs} epochs'.format(run_id=run_id, epochs=epochs))
     model = unet(input_size=(size, size, 1), with_extra_sigmoid=False, **run_params)
     log_dir = op.join('logs', run_id)
