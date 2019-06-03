b4379050fe3a41c41eb56fb8c74fe651cb30c225
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-06-03 13:35:42 +0200

    added huge study script

---
+++ huge_study.sh
@@ -0,0 +1,4 @@
+python study.py -p 0 -e 50 -f huge_experiments.json -s cifar10 &&\
+python study.py -p 1 -e 50 -f huge_experiments.json -s cifar10 &&\
+python study.py -p 2 -e 50 -f huge_experiments.json -s cifar10 &&\
+python study.py -p 3 -e 50 -f huge_experiments.json -s cifar10
