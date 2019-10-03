f98a97906fe82c10b0ec5ef55b02b5ede1772817
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-03 19:06:39 +0200

    added psnr and ssim metrics for images

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,4 @@
+from skimage.measure import compare_psnr, compare_ssim
 import tensorflow as tf
 
 def keras_psnr(y_true, y_pred):
@@ -9,3 +10,14 @@ def keras_ssim(y_true, y_pred):
     max_pixel = tf.math.reduce_max(y_true)
     min_pixel = tf.math.reduce_min(y_true)
     return tf.image.ssim(y_true, y_pred, max_pixel - min_pixel)
+
+def psnr(gt, pred):
+    """ Compute Peak Signal to Noise Ratio metric (PSNR) """
+    return compare_psnr(gt, pred, data_range=gt.max() - gt.min())
+
+
+def ssim(gt, pred):
+    """ Compute Structural Similarity Index Metric (SSIM). """
+    return compare_ssim(
+        gt.transpose(1, 2, 0), pred.transpose(1, 2, 0), multichannel=True, data_range=gt.max() - gt.min()
+    )
