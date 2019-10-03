70fdbd40181707f92b294fdc30acaa3c768b862f
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-03 16:17:53 +0200

    removed past experiments files to clear the clutter

--- huge_study.sh
+++
@@ -1,4 +0,0 @@
-python study.py -p 0 -e 50 -f huge_experiments.json -s cifar10 &&\
-python study.py -p 1 -e 50 -f huge_experiments.json -s cifar10 &&\
-python study.py -p 2 -e 50 -f huge_experiments.json -s cifar10 &&\
-python study.py -p 3 -e 50 -f huge_experiments.json -s cifar10
