beae91c523587be6931114e506b8b52a6318f474
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-04 15:29:29 +0200

    corrected psnr and ssim evaluation offline for batches of images

--- evaluate.py
+++ evaluate.py
@@ -1,3 +1,4 @@
+import numpy as np
 from skimage.measure import compare_psnr, compare_ssim
 import tensorflow as tf
 
@@ -11,13 +12,38 @@ def keras_ssim(y_true, y_pred):
     min_pixel = tf.math.reduce_min(y_true)
     return tf.image.ssim(y_true, y_pred, max_pixel - min_pixel)
 
-def psnr(gt, pred):
+def psnr_single_image(gt, pred):
     """ Compute Peak Signal to Noise Ratio metric (PSNR) """
     return compare_psnr(gt, pred, data_range=gt.max() - gt.min())
 
 
-def ssim(gt, pred):
-    """ Compute Structural Similarity Index Metric (SSIM). """
+def ssim_single_image(gt, pred):
+    """ Compute Structural Similarity Index Metric (SSIM).
+
+    The images must be in HWC format
+    """
     return compare_ssim(
-        gt.transpose(1, 2, 0), pred.transpose(1, 2, 0), multichannel=True, data_range=gt.max() - gt.min()
+        gt, pred, multichannel=True, data_range=gt.max() - gt.min()
     )
+
+def psnr(gts, preds):
+    """Compute the psnr of a batch of images in HWC format.
+
+    Images must be in NHWC format
+    """
+    if len(gts.shape) == 3:
+        return psnr_single_image(gts, preds)
+    else:
+        mean_psnr = np.mean([psnr_single_image(gt, pred) for gt, pred in zip(gts, preds)])
+        return mean_psnr
+
+def ssim(gts, preds):
+    """Compute the ssim of a batch of images in HWC format.
+
+    Images must be in NHWC format
+    """
+    if len(gts.shape) == 3:
+        return ssim_single_image(gts, preds)
+    else:
+        mean_ssim = np.mean([ssim_single_image(gt, pred) for gt, pred in zip(gts, preds)])
+        return mean_ssim
