6a4211719210c3a379543024d69a53079870e597
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-04 14:00:51 +0200

    added learned wavelet module

--- learned_wavelet.py
+++ learned_wavelet.py
@@ -1,8 +1,63 @@
-image = Input((None, None, n_channel))
+from keras.layers import Conv2D, MaxPooling2D, concatenate, Dropout, UpSampling2D, Input, AveragePooling2D, BatchNormalization, Lambda
+from keras.models import Model
+from keras.optimizers import Adam
 
-details_thresholded = Conv2D(activation='relu')(image)
-coarse = Conv2D(activation='linear')(image)
+from evaluate import keras_psnr, keras_ssim
 
-reconstructed_coarse = smthg(coarse)
+def learned_wavelet_rec(image, n_scales=1, n_details=3, n_coarse=1, n_groupping=3):
+    n_channel = image.shape[-1]
+    details_thresholded = Conv2D(
+        n_details,
+        3,
+        activation='relu',
+        padding='same',
+        kernel_initializer='he_normal',
+    )(image)
+    coarse = Conv2D(
+        n_coarse,
+        3,
+        activation='linear',
+        padding='same',
+        kernel_initializer='he_normal',
+    )(image)
+    coarse_down_sampled = AveragePooling2D()(coarse)
+    if n_scales > 1:
+        denoised_coarse = learned_wavelet_rec(coarse_down_sampled, n_scales=n_scales-1)
+    else:
+        # NOTE: potentially allow to have thresholding (i.e. non linearity) also on the coarse
+        # scale
+        denoised_coarse = coarse_down_sampled
+    denoised_coarse_upsampled = UpSampling2D(size=(2, 2))(denoised_coarse)
+    denoised_image = Conv2D(
+        n_groupping * n_channel,
+        3,
+        activation='linear',
+        padding='same',
+        kernel_initializer='he_normal',
+    )(concatenate([denoised_coarse_upsampled, details_thresholded]))
+    denoised_image = Conv2D(
+        n_channel,
+        1,
+        activation='linear',
+        padding='same',
+        kernel_initializer='he_normal',
+    )(denoised_image)
+    return denoised_image
 
-reconstructed_image = Conv2D(activation='linear')(concatenate([reconstructed_coarse, details_thresholded]))
+
+def learned_wavelet(input_size, lr, n_scales=4, n_details=3, n_coarse=1, n_groupping=3):
+    image = Input(input_size)
+    denoised_image = learned_wavelet_rec(
+        image,
+        n_scales=n_scales,
+        n_details=n_details,
+        n_coarse=n_coarse,
+        n_groupping=n_groupping,
+    )
+    model = Model(inputs=image, outputs=denoised_image)
+    model.compile(
+        optimizer=Adam(lr=lr, clipnorm=1.),
+        loss='mean_squared_error',
+        metrics=[keras_psnr, keras_ssim],
+    )
+    return model
