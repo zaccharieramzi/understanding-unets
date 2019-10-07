66b753b40488b5fd130a38f2813d73fcee671ae2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-07 17:06:01 +0200

    added a metric object like in fastmri bench to ease validation

--- evaluate.py
+++ evaluate.py
@@ -1,4 +1,5 @@
 import numpy as np
+from runstats import Statistics
 from skimage.measure import compare_psnr, compare_ssim
 import tensorflow as tf
 
@@ -47,3 +48,41 @@ def ssim(gts, preds):
     else:
         mean_ssim = np.mean([ssim_single_image(gt, pred) for gt, pred in zip(gts, preds)])
         return mean_ssim
+
+METRIC_FUNCS = dict(
+    PSNR=psnr,
+    SSIM=ssim,
+)
+
+
+class Metrics:
+    """
+    Maintains running statistics for a given collection of metrics.
+    """
+
+    def __init__(self):
+        self.metrics = {
+            metric: Statistics() for metric in METRIC_FUNCS
+        }
+
+    def push(self, target, recons):
+        for metric, func in METRIC_FUNCS.items():
+            self.metrics[metric].push(func(target, recons))
+
+    def means(self):
+        return {
+            metric: stat.mean() for metric, stat in self.metrics.items()
+        }
+
+    def stddevs(self):
+        return {
+            metric: stat.stddev() for metric, stat in self.metrics.items()
+        }
+
+    def __repr__(self):
+        means = self.means()
+        stddevs = self.stddevs()
+        metric_names = sorted(list(means))
+        return ' '.join(
+            f'{name} = {means[name]:.4g} +/- {2 * stddevs[name]:.4g}' for name in metric_names
+        )
