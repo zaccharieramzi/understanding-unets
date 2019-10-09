4ff292cd35f22f77024dbe08b234200c5c6b15a7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-09 10:47:19 +0200

    replaced old convolutions with new separate one where we can see the output before the activation

--- learning_wavelets/learned_wavelet.py
+++ learning_wavelets/learned_wavelet.py
@@ -1,25 +1,14 @@
-from keras.layers import Conv2D, concatenate, UpSampling2D, Input, AveragePooling2D
+from keras.layers import concatenate, UpSampling2D, Input, AveragePooling2D
 from keras.models import Model
 from keras.optimizers import Adam
 
 from .evaluate import keras_psnr, keras_ssim
+from .net_utils import conv_2d
 
 def learned_wavelet_rec(image, n_scales=1, n_details=3, n_coarse=1, n_groupping=3):
     n_channel = int(image.shape[-1])
-    details_thresholded = Conv2D(
-        n_details,
-        3,
-        activation='relu',
-        padding='same',
-        kernel_initializer='glorot_uniform',
-    )(image)
-    coarse = Conv2D(
-        n_coarse,
-        3,
-        activation='linear',
-        padding='same',
-        kernel_initializer='glorot_uniform',
-    )(image)
+    details_thresholded = conv_2d(image, n_details)
+    coarse = conv_2d(image, n_coarse, activation='linear')
     if n_scales > 1:
         coarse_down_sampled = AveragePooling2D()(coarse)
         denoised_coarse = learned_wavelet_rec(
@@ -34,20 +23,12 @@ def learned_wavelet_rec(image, n_scales=1, n_details=3, n_coarse=1, n_groupping=
         # NOTE: potentially allow to have thresholding (i.e. non linearity) also on the coarse
         # scale
         denoised_coarse_upsampled = coarse
-    denoised_image = Conv2D(
+    denoised_image = conv_2d(
+        concatenate([denoised_coarse_upsampled, details_thresholded]),
         n_groupping * n_channel,
-        3,
         activation='linear',
-        padding='same',
-        kernel_initializer='glorot_uniform',
-    )(concatenate([denoised_coarse_upsampled, details_thresholded]))
-    denoised_image = Conv2D(
-        n_channel,
-        1,
-        activation='linear',
-        padding='same',
-        kernel_initializer='glorot_uniform',
-    )(denoised_image)
+    )
+    denoised_image = conv_2d(denoised_image, n_channel, kernel_size=1, activation='linear')
     return denoised_image
 
 
