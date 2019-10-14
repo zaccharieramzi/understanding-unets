cb085c8764693597b136304be3d56e66a59bd7a4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-14 17:10:51 +0200

    added wavelet pooling in conv utils

--- learning_wavelets/keras_utils/conv.py
+++ learning_wavelets/keras_utils/conv.py
@@ -1,4 +1,5 @@
-from keras.layers import Activation, Conv2D
+from keras.layers import Activation, Conv2D, AveragePooling, UpSampling2D
+import numpy as np
 
 def conv_2d(image, n_channels, kernel_size=3, activation='relu'):
     image = Conv2D(
@@ -10,3 +11,29 @@ def conv_2d(image, n_channels, kernel_size=3, activation='relu'):
     )(image)
     image = Activation(activation)(image)
     return image
+
+def wavelet_pooling(image, wav_h_filter=None):
+    # TODO: refactor to have it as a shared weight in the network
+    # less memory (even if not very large)
+    def kernel_initializer():
+        return wav_h_filter[None, None, ...]
+    if wav_h_filter is None:
+        base_filter = [1/16, 1/4, 3/8, 1/4, 1/16]
+        wav_h_filter = np.array([
+            [i * j for j in base_filter]
+            for i in base_filter
+        ])
+    conv_h = Conv2D(
+        1,
+        # TODO: check that wav_h_filter is square
+        wav_h_filter.shape[0],
+        activation='linear',
+        padding='same',
+        kernel_initializer=kernel_initializer,
+    )
+    conv_h.trainable = False
+    low_freqs = conv_h(image)
+    low_freqs_downsampled = AveragePooling()(low_freqs)
+    low_freqs_down_up = UpSampling2D()(low_freqs_downsampled)
+    high_freqs = image - low_freqs_down_up
+    return [low_freqs, high_freqs]
