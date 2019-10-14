a7e1e7ce668145503e3bc639e8674947140118cc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-14 10:36:32 +0200

    added possibility to specify denoising activation

--- learning_wavelets/learned_wavelet.py
+++ learning_wavelets/learned_wavelet.py
@@ -5,9 +5,9 @@ from keras.optimizers import Adam
 from .evaluate import keras_psnr, keras_ssim
 from .net_utils import conv_2d
 
-def learned_wavelet_rec(image, n_scales=1, n_details=3, n_coarse=1, n_groupping=3):
+def learned_wavelet_rec(image, n_scales=1, n_details=3, n_coarse=1, n_groupping=3, denoising_activation='relu'):
     n_channel = int(image.shape[-1])
-    details_thresholded = conv_2d(image, n_details)
+    details_thresholded = conv_2d(image, n_details, activation=denoising_activation)
     coarse = conv_2d(image, n_coarse, activation='linear')
     if n_scales > 1:
         coarse_down_sampled = AveragePooling2D()(coarse)
@@ -32,7 +32,7 @@ def learned_wavelet_rec(image, n_scales=1, n_details=3, n_coarse=1, n_groupping=
     return denoised_image
 
 
-def learned_wavelet(input_size, lr=1e-4, n_scales=4, n_details=3, n_coarse=1, n_groupping=3):
+def learned_wavelet(input_size, lr=1e-4, n_scales=4, n_details=3, n_coarse=1, n_groupping=3, denoising_activation='relu'):
     image = Input(input_size)
     denoised_image = learned_wavelet_rec(
         image,
@@ -40,6 +40,7 @@ def learned_wavelet(input_size, lr=1e-4, n_scales=4, n_details=3, n_coarse=1, n_
         n_details=n_details,
         n_coarse=n_coarse,
         n_groupping=n_groupping,
+        denoising_activation=denoising_activation,
     )
     model = Model(inputs=image, outputs=denoised_image)
     model.compile(
