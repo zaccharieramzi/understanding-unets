58943ad29c7a42b61aa00c7023207f6e898c1e16
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-18 18:49:00 +0200

    corrected denoising activation passing

--- learning_wavelets/learned_wavelet.py
+++ learning_wavelets/learned_wavelet.py
@@ -43,6 +43,7 @@ def learned_wavelet_rec(
             n_details=n_details,
             n_coarse=n_coarse,
             n_groupping=n_groupping,
+            denoising_activation=denoising_activation,
         )
         denoised_coarse_upsampled = UpSampling2D(size=(2, 2))(denoised_coarse)
     else:
