c13bdb1315b9a1d6c2e71e5d3c7f374bbd9438d2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-10-23 15:09:00 +0200

    corrected wav pooling, to make it exact in high freq

--- learning_wavelets/keras_utils/conv.py
+++ learning_wavelets/keras_utils/conv.py
@@ -19,30 +19,47 @@ def conv_2d(image, n_channels, kernel_size=3, activation='relu', bias=True, norm
     image = Activation(activation)(image)
     return image
 
-def wavelet_pooling(image, wav_h_filter=None):
+def wavelet_pooling(image, wav_h_filter=None, wav_g_filter=None):
     # TODO: refactor to have it as a shared weight in the network
     # less memory (even if not very large)
-    def kernel_initializer(shape, **kwargs):
-        # TODO: check that shape is correspdonding
-        return wav_h_filter[..., None, None]
     if wav_h_filter is None:
         base_filter = [1/16, 1/4, 3/8, 1/4, 1/16]
         wav_h_filter = np.array([
             [i * j for j in base_filter]
             for i in base_filter
         ])
+    filter_shape = wav_h_filter.shape
+    if wav_g_filter is None:
+        filter_center_idx = filter_shape[0]//2
+        wav_g_filter = -wav_h_filter
+        wav_g_filter[filter_center_idx, filter_center_idx] = 0
+        wav_g_filter[filter_center_idx, filter_center_idx] = -np.sum(wav_g_filter)
+    def h_kernel_initializer(shape, **kwargs):
+        # TODO: check that shape is correspdonding
+        return wav_h_filter[..., None, None]
+    def g_kernel_initializer(shape, **kwargs):
+        # TODO: check that shape is correspdonding
+        return wav_g_filter[..., None, None]
     conv_h = Conv2D(
         1,
         # TODO: check that wav_h_filter is square
         wav_h_filter.shape[0],
         activation='linear',
         padding='same',
-        kernel_initializer=kernel_initializer,
+        kernel_initializer=h_kernel_initializer,
         bias=False,
     )
     conv_h.trainable = False
+    conv_g = Conv2D(
+        1,
+        # TODO: check that wav_g_filter is square
+        wav_g_filter.shape[0],
+        activation='linear',
+        padding='same',
+        kernel_initializer=g_kernel_initializer,
+        bias=False,
+    )
+    conv_g.trainable = False
     low_freqs = conv_h(image)
-    low_freqs_downsampled = AveragePooling2D()(low_freqs)
-    low_freqs_down_up = UpSampling2D()(low_freqs_downsampled)
-    high_freqs = Lambda(lambda x: x[0] - x[1])([image, low_freqs_down_up])
+    high_freqs = conv_g(image)
     return [low_freqs, high_freqs]
