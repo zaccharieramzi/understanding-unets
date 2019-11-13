a55e61c09073267466195ce26ebbf822c0d31a89
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-11-13 19:56:13 +0100

    added sparse2d wavelet denoising

--- learning_wavelets/wavelet_denoising.py
+++ learning_wavelets/wavelet_denoising.py
@@ -1,6 +1,27 @@
 from modopt.signal.wavelet import get_mr_filters, filter_convolve
 import numpy as np
+from pysap.extensions.sparse2d import Filter
 
+
+def wavelet_denoising_pysap(noisy_images, noise_std, wavelet_id='2', n_scales=2, soft_thresh=True, n_sigma=2):
+    if soft_thresh:
+        type_of_filtering = 2
+    else:
+        type_of_filtering = 1
+    flt = Filter(
+        type_of_filtering=type_of_filtering,
+        type_of_multiresolution_transform=float(wavelet_id),
+        sigma_noise=noise_std,
+        number_of_scales=n_scales,
+        verbose=0,
+        tab_n_sigma=n_sigma,
+    )
+    denoised_images = list()
+    for noisy_image in noisy_images:
+        flt.filter(noisy_image)
+        denoised_image = flt.data
+        denoised_images.append(denoised_image)
+    return denoised_images
 
 def threshold_wavelet_coefficients(data, wav_filters, noise_std):
     threshold_per_level = np.empty((len(wav_filters), *data.shape))
