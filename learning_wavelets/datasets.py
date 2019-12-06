d470b21becd00305d2e5dafc7c8b22b5f829af55
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2019-12-06 11:19:44 +0100

    allowed multiple noise levels in training dataset

--- learning_wavelets/datasets.py
+++ learning_wavelets/datasets.py
@@ -1,3 +1,5 @@
+from collections.abc import Iterable
+
 import numpy as np
 import scipy.ndimage as ndimage
 import tensorflow as tf
@@ -32,16 +34,27 @@ def select_patch_in_image_function(patch_size, seed=0):
     return select_patch_in_image
 
 # noise
-def add_noise_function(noise_std):
+def add_noise_function(noise_std_range, return_noise_level=False):
+    if not isinstance(noise_std_range, Iterable):
+        noise_std_range = (noise_std_range, noise_std_range)
+    @tf.py_function
     def add_noise(image):
+        noise_std = tf.random.uniform(
+            (1,),
+            minval=noise_std_range[0],
+            maxval=noise_std_range[1],
+        )
         noise = tf.random.normal(shape=tf.shape(image), mean=0.0, stddev=noise_std/255, dtype=tf.float32)
-        return image + noise
+        if return_noise_level:
+            return (image + noise), noise_std
+        else:
+            return image + noise
     return add_noise
 
 def exact_recon_helper(image_noisy, image):
     return (image_noisy, image), (image, image)
 
-def im_dataset_div2k(mode='training', batch_size=1, patch_size=256, noise_std=30, exact_recon=False):
+def im_dataset_div2k(mode='training', batch_size=1, patch_size=256, noise_std=30, exact_recon=False, return_noise_level=False):
     if mode == 'training':
         path = 'DIV2K_train_HR'
     elif mode == 'validation':
@@ -62,12 +75,13 @@ def im_dataset_div2k(mode='training', batch_size=1, patch_size=256, noise_std=30
     image_patch_ds = image_grey_ds.map(
         select_patch_in_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
-    add_noise = add_noise_function(noise_std)
+    add_noise = add_noise_function(noise_std, return_noise_level=return_noise_level)
     image_noisy_ds = image_patch_ds.map(
         lambda patch: (add_noise(patch), patch),
         num_parallel_calls=tf.data.experimental.AUTOTUNE,
     )
     if exact_recon:
+        # TODO: see how to adapt exact recon for the case of noise level included
         image_noisy_ds = image_noisy_ds.map(
             exact_recon_helper,
             num_parallel_calls=tf.data.experimental.AUTOTUNE,
