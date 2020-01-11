132e36adc3f3c41820cb4bc68f9947f73f45fe61
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-01-11 15:19:43 +0100

    added possibility to remove bn in dncnn

--- learning_wavelets/dncnn.py
+++ learning_wavelets/dncnn.py
@@ -5,7 +5,7 @@ from tensorflow.keras.optimizers import Adam
 from .evaluate import keras_psnr, keras_ssim
 
 
-def dncnn(input_size=(None, None, 1), filters=64, depth=20, lr=1e-3):
+def dncnn(input_size=(None, None, 1), filters=64, depth=20, lr=1e-3, bn=True):
     # the code is from https://github.com/cszn/DnCNN/blob/master/TrainingCodes/dncnn_keras/main_train.py
     inpt = Input(shape=input_size)
     # 1st layer, Conv+relu
@@ -24,7 +24,8 @@ def dncnn(input_size=(None, None, 1), filters=64, depth=20, lr=1e-3):
             padding='same',
             use_bias=False,
         )(x)
-        x = BatchNormalization(axis=-1, epsilon=1e-3, momentum=0.9)(x)
+        if bn:
+            x = BatchNormalization(axis=-1, epsilon=1e-3, momentum=0.9)(x)
         x = Activation('relu')(x)
     x = Conv2D(
         filters=input_size[-1],
