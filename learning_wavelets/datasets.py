f94bd35d08dbd07b714cab16fb73eea94025ed0a
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-01-23 17:25:57 +0100

    added the possibility to have padding in case of pooling in the bsd dataset

--- learning_wavelets/datasets.py
+++ learning_wavelets/datasets.py
@@ -36,6 +36,17 @@ def select_patch_in_image_function(patch_size, seed=0):
             return image
     return select_patch_in_image
 
+# padding
+def pad_for_pool(n_pooling=1):
+    def pad(image):
+        im_shape = tf.shape(image)[:2]
+        to_pad = (tf.dtypes.cast(im_shape / 2**n_pooling, 'int32') + 1) * 2**n_pooling - im_shape
+        # the + 1 is necessary because the images have odd shapes
+        pad_seq = [(to_pad[0]//2, to_pad[0]//2 + 1), (to_pad[1]//2, to_pad[1]//2 + 1), (0, 0)]
+        image_padded = tf.pad(image, pad_seq, 'SYMMETRIC')
+        return image_padded
+    return pad
+
 # noise
 def add_noise_function(noise_std_range, return_noise_level=False, no_noise=False):
     if not isinstance(noise_std_range, Iterable):
@@ -98,7 +109,7 @@ def im_dataset_div2k(mode='training', batch_size=1, patch_size=256, noise_std=30
     image_noisy_ds = image_noisy_ds.batch(batch_size).repeat().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     return image_noisy_ds
 
-def im_dataset_bsd500(mode='training', batch_size=1, patch_size=256, noise_std=30, exact_recon=False, no_noise=False, return_noise_level=False):
+def im_dataset_bsd500(mode='training', batch_size=1, patch_size=256, noise_std=30, exact_recon=False, no_noise=False, return_noise_level=False, n_pooling=None):
     # the training set for bsd500 is test + train
     # the test set (i.e. containing bsd68 images) is val
     if mode == 'training':
@@ -123,10 +134,18 @@ def im_dataset_bsd500(mode='training', batch_size=1, patch_size=256, noise_std=3
         normalise, num_parallel_calls=tf.data.experimental.AUTOTUNE
     )
     # image_grey_aug_ds = image_grey_ds.map(tf_random_rotate_image)
-    select_patch_in_image = select_patch_in_image_function(patch_size)
-    image_patch_ds = image_grey_ds.map(
-        select_patch_in_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
-    )
+    if patch_size is not None:
+        select_patch_in_image = select_patch_in_image_function(patch_size)
+        image_patch_ds = image_grey_ds.map(
+            select_patch_in_image, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+    elif n_pooling is not None:
+        pad = pad_for_pool(n_pooling)
+        image_patch_ds = image_grey_ds.map(
+            pad, num_parallel_calls=tf.data.experimental.AUTOTUNE
+        )
+    else:
+        image_patch_ds = image_grey_ds
     add_noise = add_noise_function(noise_std, return_noise_level=return_noise_level, no_noise=no_noise)
     image_noisy_ds = image_patch_ds.map(
         lambda patch: (add_noise(patch), patch),
