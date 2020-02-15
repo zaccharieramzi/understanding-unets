a675493f714b678796f66c351bdf46f12d5e7ca1
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-15 12:32:25 +0100

    expanded tensors for computations in graph mode

--- learning_wavelets/learnlet_model.py
+++ learning_wavelets/learnlet_model.py
@@ -86,7 +86,9 @@ class Learnlet(Model):
             for weight, learnlet_analysis_coeff_thresholded, thresholding_layer in zip(
                     weights, learnlet_analysis_coeffs_thresholded, self.threshold.thresholding_layers
                 ):
-                actual_threshold = noise_std * thresholding_layer.alpha * weight
+                expanded_noise = tf.expand_dims(tf.expand_dims(noise_std, axis=1), axis=1)
+                expanded_alpha =  tf.expand_dims(tf.expand_dims(tf.expand_dims(thresholding_layer.alpha, axis=0), axis=0), axis=0)
+                actual_threshold =  expanded_noise * expanded_alpha * weight
                 new_weight = weight / (1 + tf.math.abs(learnlet_analysis_coeff_thresholded) / actual_threshold)
                 new_weights.append(new_weight)
             weights = new_weights
