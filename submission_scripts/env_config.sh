433d857fc6ea9104db90a03fb964a0a8dd7cc2a4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-19 15:55:04 +0100

    added mkl as a module to load in the submission scripts

--- submission_scripts/env_config.sh
+++ submission_scripts/env_config.sh
@@ -4,6 +4,7 @@ module load feature/openmpi/net/ib/openib
 module load cuda/10.1.105
 module load python3/3.7.5
 module load gnu/8.3.0
+module load mkl/19.0.5.281
 
 pip install --target=$CCCWORKDIR/installed-packages/ --upgrade ./
 
