3f0e5b69a9891a6491a2e87888db9a5cd5ef82be
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-02-24 23:38:06 +0100

    refactored subclassed model tests and added fit test

--- learning_wavelets/tests/models/learnlet_model_test.py
+++ learning_wavelets/tests/models/learnlet_model_test.py
@@ -1,20 +1,22 @@
 import pytest
+import tensorflow as tf
+import tensorflow.keras.backend as K
 
 from learning_wavelets.models.learnlet_model import Learnlet
 
-@pytest.mark.parametrize('learnlet_kwargs',[
+learnlet_test_cases = [
     {},
     {'n_reweights_learn': 3},
-    {'exact_reconstruction': True},
-])
+    # TODO: maybe change in subclassed model to have a check for these 2 params when doing exact recon
+    {'exact_reconstruction': True, 'learnlet_synthesis_kwargs': {'res': True}, 'learnlet_analysis_kwargs': {'skip_connection': True}},
+]
+
+@pytest.mark.parametrize('learnlet_kwargs', learnlet_test_cases)
 def test_init(learnlet_kwargs):
     Learnlet(**learnlet_kwargs)
+    K.clear_session()
 
-@pytest.mark.parametrize('learnlet_kwargs',[
-    {},
-    {'n_reweights_learn': 3},
-    {'exact_reconstruction': True},
-])
+@pytest.mark.parametrize('learnlet_kwargs', learnlet_test_cases)
 def test_build_compile(learnlet_kwargs):
     model = Learnlet(**learnlet_kwargs)
     model.compile(
@@ -22,5 +24,20 @@ def test_build_compile(learnlet_kwargs):
         loss='mse',
     )
     model.build([(None, 32, 32, 1), (None, 1)])
+    K.clear_session()
+
+@pytest.mark.parametrize('learnlet_kwargs', learnlet_test_cases)
+def test_fit(learnlet_kwargs):
+    model = Learnlet(**learnlet_kwargs)
+    model.compile(
+        optimizer='adam',
+        loss='mse',
+    )
+    model.build([(None, 32, 32, 1), (None, 1)])
+    model.fit(
+        x=[tf.random.normal((8, 32, 32, 1), seed=0), tf.zeros((8, 1))],
+        y=tf.random.normal((8, 32, 32, 1), seed=0),
+    )
+    K.clear_session()
 
 # TODO: add test for exact reconstruction
