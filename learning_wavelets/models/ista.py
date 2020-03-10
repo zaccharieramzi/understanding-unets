78987e0d9dca9baaface918c459d5106858244a4
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-03-10 13:08:35 +0100

    corrected prox op in ista block and made the ista network compatible with non complex type

--- learning_wavelets/models/ista.py
+++ learning_wavelets/models/ista.py
@@ -6,12 +6,13 @@ from .learnlet_model import Learnlet
 
 class IstaLearnlet(Model):
     __name__ = 'ista_learnlet'
-    def __init__(self, n_iterations, forward_operator, adjoint_operator, operator_lips_cst=None, postprocess=None, **learnlet_kwargs):
+    def __init__(self, n_iterations, forward_operator, adjoint_operator, operator_lips_cst=None, postprocess=None, complex_mode=True, **learnlet_kwargs):
         super(IstaLearnlet, self).__init__()
         self.n_iterations = n_iterations
         self.forward_operator = forward_operator
         self.adjoint_operator = adjoint_operator
         self.postprocess = postprocess
+        self.complex_mode = complex_mode
         self.learnlet = Learnlet(**learnlet_kwargs)
         self.ista_blocks = [
             IstaLayer(
@@ -29,7 +30,6 @@ class IstaLearnlet(Model):
         for ista_block in self.ista_blocks:
             # ISTA-step
             x = ista_block([x, inputs])
-        x = tf.math.abs(x)
         if self.postprocess is not None:
             x = self.postprocess(x)
         return x
@@ -56,10 +56,7 @@ class IstaLayer(Layer):
         grad = self.grad(x, inputs)
         alpha = tf.cast(self.alpha, grad.dtype)
         x = x - alpha * grad
-        # learnlet transform on both the real and imaginary part
-        x_real = self.prox(tf.math.real(x), self.alpha)
-        x_imag = self.prox(tf.math.imag(x), self.alpha)
-        x = tf.complex(x_real, x_imag)
+        x = self.prox_alpha(x)
         return x
 
     def grad(self, x, inputs):
@@ -68,5 +65,12 @@ class IstaLayer(Layer):
         measurements_residual = self.forward_operator(x[..., 0], subsampling_pattern) - measurements
         return self.adjoint_operator(measurements_residual, subsampling_pattern)[..., None]
 
-    def prox(self, x, alpha):
-        return self.learnlet([x, alpha])
+    def prox_alpha(self, x):
+        # learnlet transform on both the real and imaginary part
+        if self.complex_mode:
+            x_real = self.learnlet([tf.math.real(x), self.alpha])
+            x_imag = self.learnlet([tf.math.imag(x), self.alpha])
+            x = tf.complex(x_real, x_imag)
+        else:
+            x = self.learnlet([x, self.alpha])
+        return x
