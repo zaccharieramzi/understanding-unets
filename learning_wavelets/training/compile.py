d88da06e8b83631d07ed18d5fbc841b8b3370dd2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-26 10:28:09 +0200

    made the compilation link the temporary evaluation module

--- learning_wavelets/training/compile.py
+++ learning_wavelets/training/compile.py
@@ -1,6 +1,6 @@
 import tensorflow_addons as tfa
 
-from learning_wavelets.evaluate.metrics import keras_psnr, keras_ssim
+from learning_wavelets.evaluate_tmp.metrics import keras_psnr, keras_ssim
 
 def default_model_compile(model, lr=1e-3):
     model.compile(
