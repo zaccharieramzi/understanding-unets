456a32cac000d7901ae840aab2fbf9d1c1e024fc
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-26 15:35:51 +0200

    added baseline evaluations (original, wav, bm3d)

---
+++ learning_wavelets/evaluate_tmp/baseline_eval.py
@@ -0,0 +1,77 @@
+import bm3d
+import numpy as np
+from tqdm import tqdm
+
+from .multiscale_eval import DEFAULT_NOISE_STDS
+from .results_to_csv import results_to_csv
+from .runstats import Metrics, METRIC_FUNCS
+from learning_wavelets.data.datasets import im_dataset_bsd68
+from learning_wavelets.wavelet_denoising import wavelet_denoising_pysap
+
+
+def metrics_baseline(denoising_function, baseline_name='original', n_pooling=5, **kwargs):
+    metrics_list = []
+    for noise_std in DEFAULT_NOISE_STDS:
+        metrics = Metrics()
+        im_ds = im_dataset_bsd68(
+            mode='testing',
+            batch_size=1,
+            patch_size=None,
+            noise_std=noise_std,
+            return_noise_level=True,
+            n_samples=None,
+            n_pooling=n_pooling,
+        )
+        for images_noisy, images_gt, im_shape in tqdm(im_ds, desc='Stats for original noisy images'):
+            images_denoised = denoising_function(images_noisy, noise_std, **kwargs)
+            metrics.push(images_gt.numpy(), images_denoised, im_shape.numpy())
+        metrics_list.append(metrics)
+    metrics_names = METRIC_FUNCS.keys()
+    formatted_metrics_list = [
+        m.means().values()
+        for m in metrics_list
+    ]
+    results_to_csv(
+        metrics_names,
+        [kwargs, formatted_metrics_list],
+        f'{baseline_name}_metrics.csv',
+    )
+    return metrics_list
+
+def metrics_original():
+    def identity(images_noisy, noise_std):
+        return images_noisy.numpy()
+    return metrics_baseline(identity)
+
+
+def metrics_wavelets_from_ds(wavelet_id='24'):
+    soft_thresh = False
+    def wavelet_denoise(
+            images_noisy,
+            noise_std,
+            wavelet_id=wavelet_id,
+            soft_thresh=False
+        ):
+        return np.array(wavelet_denoising_pysap(
+            images_noisy[..., 0].numpy(),
+            noise_std=noise_std/255,
+            wavelet_id=wavelet_id,
+            n_scales=5,
+            soft_thresh=soft_thresh,
+            n_sigma=3,
+        ))[..., None]
+    return metrics_baseline(
+        wavelet_denoise,
+        baseline_name='wavelets',
+        soft_thresh=soft_thresh,
+        wavelet_id=wavelet_id,
+    )
+
+def metrics_bm3d():
+    def bm3d_denoise(images_noisy, noise_std):
+        return bm3d.bm3d(
+            images_noisy[0, ..., 0].numpy() + 0.5,
+            sigma_psd=noise_std/255,
+            stage_arg=bm3d.BM3DStages.ALL_STAGES
+        )[None, ..., None] - 0.5
+    return metrics_baseline(bm3d_denoise, baseline_name='bm3d')
---
+++ learning_wavelets/evaluate_tmp/runstats.py
@@ -0,0 +1,82 @@
+import numpy as np
+from runstats import Statistics
+from skimage.measure import compare_psnr, compare_ssim
+
+from learning_wavelets.image_utils import trim_padding
+
+
+def psnr_single_image(gt, pred):
+    """ Compute Peak Signal to Noise Ratio metric (PSNR) """
+    return compare_psnr(gt, pred, data_range=1)
+
+
+def ssim_single_image(gt, pred):
+    """ Compute Structural Similarity Index Metric (SSIM).
+
+    The images must be in HWC format
+    """
+    return compare_ssim(
+        gt, pred, multichannel=True, data_range=1
+    )
+
+def psnr(gts, preds):
+    """Compute the psnr of a batch of images in HWC format.
+
+    Images must be in NHWC format
+    """
+    if len(gts.shape) == 3:
+        return psnr_single_image(gts, preds)
+    else:
+        mean_psnr = np.mean([psnr_single_image(gt, pred) for gt, pred in zip(gts, preds)])
+        return mean_psnr
+
+def ssim(gts, preds):
+    """Compute the ssim of a batch of images in HWC format.
+
+    Images must be in NHWC format
+    """
+    if len(gts.shape) == 3:
+        return ssim_single_image(gts, preds)
+    else:
+        mean_ssim = np.mean([ssim_single_image(gt, pred) for gt, pred in zip(gts, preds)])
+        return mean_ssim
+
+METRIC_FUNCS = dict(
+    PSNR=psnr,
+    SSIM=ssim,
+)
+
+
+class Metrics:
+    """
+    Maintains running statistics for a given collection of metrics.
+    """
+
+    def __init__(self):
+        self.metrics = {
+            metric: Statistics() for metric in METRIC_FUNCS
+        }
+
+    def push(self, target, recons, im_shape=None):
+        if im_shape is not None:
+            target, recons = trim_padding(im_shape, target, recons)
+        for metric, func in METRIC_FUNCS.items():
+            self.metrics[metric].push(func(target, recons))
+
+    def means(self):
+        return {
+            metric: stat.mean() for metric, stat in self.metrics.items()
+        }
+
+    def stddevs(self):
+        return {
+            metric: stat.stddev() for metric, stat in self.metrics.items()
+        }
+
+    def __repr__(self):
+        means = self.means()
+        stddevs = self.stddevs()
+        metric_names = sorted(list(means))
+        return ' '.join(
+            f'{name} = {means[name]:.4g} +/- {2 * stddevs[name]:.4g}' for name in metric_names
+        )
