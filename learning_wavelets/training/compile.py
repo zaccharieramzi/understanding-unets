e71f8922676994d3ba10a1dfc9409d6644051263
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-26 18:36:12 +0200

    removed gradient clipping as not working in distributed setting

--- learning_wavelets/training/compile.py
+++ learning_wavelets/training/compile.py
@@ -4,7 +4,7 @@ from learning_wavelets.evaluate_tmp.metrics import keras_psnr, keras_ssim
 
 def default_model_compile(model, lr=1e-3):
     model.compile(
-        optimizer=tfa.optimizers.RectifiedAdam(lr=lr, clipnorm=1.),
+        optimizer=tfa.optimizers.RectifiedAdam(lr=lr),
         loss='mean_squared_error',
         metrics=[keras_psnr, keras_ssim],
     )
