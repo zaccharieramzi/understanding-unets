c7300174fcd1a31b601b5a2ddf6a755f77400b7c
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-27 10:53:15 +0200

    cleaned env config for faster start of jobs

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 module purge
-module load python/3.7.5
 module load tensorflow-gpu/py3/2.2.0-dev
-
-export TMPDIR=$SCRATCH/tmp
-pip install --no-cache-dir ./
-pip install -r learning_wavelets/requirements.txt
 
 export BSD500_DATA_DIR=$SCRATCH/
 export BSD68_DATA_DIR=$SCRATCH/
