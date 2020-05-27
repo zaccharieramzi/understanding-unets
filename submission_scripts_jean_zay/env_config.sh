d2ab3c3f29fd803a22b151d822ddca3b5430cdcf
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-27 10:28:45 +0200

    switched to tf 2.2.0 for jz

--- submission_scripts_jean_zay/env_config.sh
+++ submission_scripts_jean_zay/env_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 module purge
 module load python/3.7.5
-module load tensorflow-gpu/py3/2.1.0
+module load tensorflow-gpu/py3/2.2.0-dev
 
 export TMPDIR=$SCRATCH/tmp
 pip install --no-cache-dir ./
