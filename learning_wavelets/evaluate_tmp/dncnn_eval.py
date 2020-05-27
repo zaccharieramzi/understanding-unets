cc71b650d5657c49e6013ba9a39acc601dfffca7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-27 09:57:54 +0200

    added a dncnn eval script

---
+++ learning_wavelets/evaluate_tmp/dncnn_eval.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+
+from learning_wavelets.config import CHECKPOINTS_DIR
+from learning_wavelets.models.dncnn import dncnn
+from .multiscale_eval import evaluate_multiscale
+
+
+def evaluate_dncnn(run_id, filters=64, depth=20, n_epochs=500, **kwargs):
+    # model definition
+    run_params = {
+        "filters": filters,
+        'depth': depth,
+        'bn': False,
+    }
+    n_channels = 1
+    mirrored_strategy = tf.distribute.MirroredStrategy()
+    with mirrored_strategy.scope():
+        model = dncnn(input_size=(None, None, n_channels), **run_params)
+    chkpt_path = f'{CHECKPOINTS_DIR}checkpoints/{run_id}-{n_epochs:02d}.hdf5'
+    model.load_weights(chkpt_path)
+    metrics = evaluate_multiscale(
+        model,
+        distrib_strat=mirrored_strategy,
+        n_scales=1,
+        dynamic_denoising=False,
+        **kwargs,
+    )
+    return metrics
