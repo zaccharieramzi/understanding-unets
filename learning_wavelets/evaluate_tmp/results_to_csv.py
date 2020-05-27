8a126b2be49b3ed28e4991750f62471589a9ab80
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-05-27 16:42:39 +0200

    corrected results to csv

--- learning_wavelets/evaluate_tmp/results_to_csv.py
+++ learning_wavelets/evaluate_tmp/results_to_csv.py
@@ -6,18 +6,18 @@ def results_to_csv(metrics_names, results, output_path):
     results_df = None
     i = 0
     for parameters, eval_res in results:
-        params_couples = sorted(list(parameters.items()), key=lambda x: x[0])
-        params_keys = [x[0] for x in params_couples]
-        params_values = [x[1] for x in params_couples]
         if results_df is None:
             results_df = pd.DataFrame(
-                columns=params_keys + ['noise_std'] + metrics_names,
+                columns=parameters.keys() + ['noise_std'] + metrics_names,
             )
         for noise_res, noise_std in zip(eval_res, DEFAULT_NOISE_STDS):
             # NOTE: not efficient as per pandas doc, but easy
-            results_df = results_df.append(
-                params_values + [noise_std] + noise_res,
-            )
+            row_dict = parameters
+            row_dict.update({
+                'noise_std': noise_std,
+            })
+            row_dict.update(dict(zip(metrics_names, noise_res)))
+            results_df = results_df.append(row_dict)
             i += 1
     results_df.to_csv(output_path)
     return results_df
