bc3df89feab95b719cc17da2f876b6cc90f52efd
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-12-04 16:47:10 +0100

    added the use of checkpoints dir in model loading

--- learning_wavelets/keras_utils/load_model.py
+++ learning_wavelets/keras_utils/load_model.py
@@ -1,5 +1,8 @@
+from learning_wavelets.config import CHECKPOINTS_DIR
+
+
 def unpack_model(init_function=None, run_params=None, run_id=None, epoch=250, **dummy_kwargs):
     model = init_function(**run_params)
-    chkpt_path = f'checkpoints/{run_id}-{epoch}.hdf5'
+    chkpt_path = f'{CHECKPOINTS_DIR}/{run_id}-{epoch}.hdf5'
     model.load_weights(chkpt_path)
     return model
