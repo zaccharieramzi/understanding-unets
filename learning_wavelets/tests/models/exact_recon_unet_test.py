113f3a86a6337e98ce15eda650e82b924775ab37
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-04-27 10:48:53 +0200

    Update exact_recon_unet_test.py

--- learning_wavelets/tests/models/exact_recon_unet_test.py
+++ learning_wavelets/tests/models/exact_recon_unet_test.py
@@ -2,15 +2,15 @@ import pytest
 import tensorflow as tf
 import tensorflow.keras.backend as K
 
-from learning_wavelets.models.unet import unet
+from learning_wavelets.models.exact_recon_unet.py import ExactReconUnet
 
 
 def test_init():
-    unet(n_layers=3, layers_n_non_lins=2)
+    (n_layers=3, layers_n_non_lins=2)
     K.clear_session()
 
 def test_fit():
-    model = unet(input_size=(None, None, 1), n_layers=3, layers_n_non_lins=2)
+    model = ExactReconUnet(input_size=(None, None, 1), n_layers=3, layers_n_non_lins=2)
     model.fit(
         x=tf.random.normal((8, 32, 32, 1), seed=0),
         y=tf.random.normal((8, 32, 32, 1), seed=0),
