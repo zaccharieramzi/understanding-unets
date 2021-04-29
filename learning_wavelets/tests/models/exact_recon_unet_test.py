c0c74d9473fb5d31c8f9c3fd504165d9bd7f478a
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-04-29 09:44:23 +0200

    Changed network arguments and added noise input

--- learning_wavelets/tests/models/exact_recon_unet_test.py
+++ learning_wavelets/tests/models/exact_recon_unet_test.py
@@ -1,18 +1,21 @@
 import pytest
 import tensorflow as tf
 import tensorflow.keras.backend as K
+from tensorflow.keras.optimizers import Adam
 
 from learning_wavelets.models.exact_recon_unet import ExactReconUnet
 
 
 def test_init():
-    ExactReconUnet(n_layers=3, layers_n_non_lins=2)
+    ExactReconUnet()
     K.clear_session()
 
 def test_fit():
-    model = ExactReconUnet(input_size=(None, None, 1), n_layers=3, layers_n_non_lins=2)
+    model=ExactReconUnet(n_output_channels=1, kernel_size=3, layers_n_channels=[4, 8, 16, 32])
+    model.compile(optimizer=Adam(lr=1e-3), loss='mse')
     model.fit(
-        x=tf.random.normal((8, 32, 32, 1), seed=0),
+        x=(tf.random.normal((8, 32, 32, 1), seed=0),
+           tf.random.normal((8, 1, 1, 1), seed=0)),
         y=tf.random.normal((8, 32, 32, 1), seed=0),
     )
     K.clear_session()
