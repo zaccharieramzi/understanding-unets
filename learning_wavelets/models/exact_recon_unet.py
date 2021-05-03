6fd8662311978bc353a97cb1b3701fe741491b92
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-05-03 18:23:12 +0200

    Added u-net with exact reconstruction

---
+++ learning_wavelets/models/exact_recon_unet.py
@@ -0,0 +1,160 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.layers import Layer, Conv2D, LeakyReLU, PReLU, UpSampling2D, MaxPooling2D, Activation
+from tensorflow.keras.models import Model
+from math import log, ceil, floor
+
+tf.config.run_functions_eagerly(True)
+
+class Conv(Layer):
+    def __init__(self, n_filters, kernel_size=3, non_linearity='relu', **kwargs):
+        super().__init__(**kwargs)
+        self.n_filters = n_filters
+        self.kernel_size = kernel_size
+        self.non_linearity = non_linearity
+        self.conv = Conv2D(
+            filters=self.n_filters,
+            kernel_size=self.kernel_size,
+            padding='same',
+            activation=None,
+        )
+        if self.non_linearity == 'lrelu':
+            self.act = LeakyReLU(0.1)
+        elif self.non_linearity == 'prelu':
+            self.act = PReLU(shared_axes=[1, 2])
+        else:
+            self.act = Activation(self.non_linearity)
+
+    def call(self, inputs):
+        outputs = self.conv(inputs)
+        outputs = self.act(outputs)
+        return outputs
+
+class ConvBlock(Layer):
+    def __init__(self, n_filters, kernel_size=3, non_linearity='relu', n_non_lins=2, **kwargs):
+        super().__init__(**kwargs)
+        self.n_filters = n_filters
+        self.kernel_size = kernel_size
+        self.non_linearity = non_linearity
+        self.n_non_lins = n_non_lins
+        self.convs = [
+            Conv(
+                n_filters=self.n_filters,
+                kernel_size=self.kernel_size,
+                non_linearity=self.non_linearity,
+            ) for _ in range(self.n_non_lins)
+        ]
+
+    def call(self, inputs):
+        outputs = inputs
+        for conv in self.convs:
+            outputs = conv(outputs)
+        return outputs
+
+class UpConv(Layer):
+    def __init__(self, n_filters, kernel_size=3, **kwargs):
+        super().__init__(**kwargs)
+        self.n_filters = n_filters
+        self.kernel_size = kernel_size
+        self.conv = Conv2D(
+            filters=self.n_filters,
+            kernel_size=self.kernel_size,
+            padding='same',
+            activation=None,
+        )
+        self.up = UpSampling2D(size=(2, 2))
+
+    def call(self, inputs):
+        outputs = self.up(inputs)
+        outputs = self.conv(outputs)
+        return outputs
+
+
+class ExactReconUnet(Model):
+    def __init__(
+            self,
+            n_output_channels=1,
+            kernel_size=3,
+            layers_n_channels=[64, 128, 256, 512, 1024],
+            layers_n_non_lins=2,
+            non_linearity='relu',
+            **kwargs,
+        ):
+        super().__init__(**kwargs)
+        self.n_output_channels = n_output_channels
+        self.kernel_size = kernel_size
+        self.layers_n_channels = layers_n_channels
+        self.n_layers = len(self.layers_n_channels)
+        self.layers_n_non_lins = layers_n_non_lins
+        self.non_linearity = non_linearity
+        self.down_convs = [
+            ConvBlock(
+                n_filters=n_channels,
+                kernel_size=self.kernel_size,
+                non_linearity=self.non_linearity,
+                n_non_lins=self.layers_n_non_lins,
+            ) for n_channels in self.layers_n_channels[:-1]
+        ]
+        self.down = MaxPooling2D(pool_size=(2, 2), padding='same')
+        self.bottom_conv = ConvBlock(
+            n_filters=self.layers_n_channels[-1],
+            kernel_size=self.kernel_size,
+            non_linearity=self.non_linearity,
+            n_non_lins=self.layers_n_non_lins,
+        )
+        self.up_convs = [
+            ConvBlock(
+                n_filters=n_channels,
+                kernel_size=self.kernel_size,
+                non_linearity=self.non_linearity,
+                n_non_lins=self.layers_n_non_lins,
+            ) for n_channels in self.layers_n_channels[:-1]
+        ]
+        self.ups = [
+            UpConv(
+                n_filters=n_channels,
+                kernel_size=self.kernel_size,
+            ) for n_channels in self.layers_n_channels[:-1]
+        ]
+        self.final_conv = Conv2D(
+            filters=self.n_output_channels,
+            kernel_size=1,
+            padding='same',
+            activation=None,
+        )
+
+    
+    def call(self, inputs):
+        scales = []
+        noisy_image = inputs[0]
+        noise_std = inputs[1]
+        outputs = noisy_image
+        outputs, crop_w, crop_h, w, h = pad_power_of_two(outputs, self.layers_n_channels)
+        for conv in self.down_convs:
+            outputs = conv(outputs)
+            scales.append(outputs)
+            outputs = self.down(outputs)
+        outputs = self.bottom_conv(outputs)
+        for scale, conv, up in zip(scales[::-1], self.up_convs[::-1], self.ups[::-1]):
+            outputs = up(outputs)
+            outputs = tf.concat([outputs, scale], axis=-1)
+            outputs = conv(outputs)
+        outputs = self.final_conv(outputs)
+        noise_std = tf.reshape(noise_std, shape=[tf.shape(noise_std)[0], 1, 1, 1])
+        outputs = tf.image.crop_to_bounding_box(outputs, int(floor(crop_w)), int(floor(crop_h)), w, h)
+        outputs = noisy_image - noise_std * outputs
+        return outputs
+    
+
+def pad_power_of_two(x, layers):
+    
+    diff = 2**len(layers)
+    y = tf.convert_to_tensor(x).numpy()
+    w = y.shape[1]
+    h = y.shape[2]
+    
+    pad_value_w = w % diff 
+    pad_value_h = h % diff 
+    padding = tf.constant([(0,0), (int(floor(pad_value_w/2)), int(ceil(pad_value_w/2))), (int(floor(pad_value_h/2)), int(ceil(pad_value_h/2))), (0, 0)])
+        
+    return tf.pad(x, padding, 'CONSTANT'), pad_value_w, pad_value_h, w, h
