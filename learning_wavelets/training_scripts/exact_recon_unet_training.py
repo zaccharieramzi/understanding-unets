cfc800ef20c8830b2f8e1a58bd5c16596289f0f3
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-05-05 09:35:07 +0200

    Changed n_samples to None
    Co-authored-by: Zaccharie Ramzi <zaccharie.ramzi@gmail.com>

--- learning_wavelets/training_scripts/exact_recon_unet_training.py
+++ learning_wavelets/training_scripts/exact_recon_unet_training.py
@@ -14,7 +14,7 @@ from learning_wavelets.models.exact_recon_unet import ExactReconUnet
 tf.random.set_seed(1)
 
 
-def train_unet(noise_std_train=(0, 55), noise_std_val=30, n_samples=2000, source='bsd500', base_n_filters=4, batch_size=8, n_epochs=50):
+def train_unet(noise_std_train=(0, 55), noise_std_val=30, n_samples=None, source='bsd500', base_n_filters=4, batch_size=8, n_epochs=50):
 
     # data preparation
     if source == 'bsd500':
