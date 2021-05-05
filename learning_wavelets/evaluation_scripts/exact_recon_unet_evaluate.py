06e76d4b085a7781c365d73c82bf1da37d1cb401
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-05-05 10:39:52 +0200

    Added possibility of testing for different noise levels
    Also de model instructions were included outside the for loop

--- learning_wavelets/evaluation_scripts/exact_recon_unet_evaluate.py
+++ learning_wavelets/evaluation_scripts/exact_recon_unet_evaluate.py
@@ -27,25 +27,30 @@ def evaluate_unet(
         'layers_n_non_lins': layers_n_non_lins,
         'non_linearity': non_linearity,
     }
-
-    val_set = im_dataset_bsd68(
-        mode='testing',
-        batch_size=1,
-        patch_size=None,
-        noise_std=noise_std_test,
-        return_noise_level=True,
-    )
-
     
     model = ExactReconUnet(**run_params)
-
+    
     inputs = [tf.zeros((1, 32, 32, 1)), tf.zeros((1, 1))]
     model(inputs)
-    
+        
     model.load_weights(f'{CHECKPOINTS_DIR}checkpoints/{run_id}-{n_epochs}.hdf5')
     
-    eval_res = Metrics()
-    for x, y_true, size in tqdm(val_set.as_numpy_iterator()):
-        y_pred = model.predict(x)
-        eval_res.push(y_true, y_pred)
-    return METRIC_FUNCS, (list(eval_res.means().values()), list(eval_res.stddevs().values()))
+    loss_noise_dic = {}
+    
+    for noise_level in noise_std_test:
+        val_set = im_dataset_bsd68(
+            mode='testing',
+            batch_size=1,
+            patch_size=None,
+            noise_std=noise_level,
+            return_noise_level=True,
+        )
+    
+        
+        eval_res = Metrics()
+        for x, y_true, size in tqdm(val_set.as_numpy_iterator()):
+            y_pred = model.predict(x)
+            eval_res.push(y_true, y_pred)
+        loss_noise_dic[noise_level] = METRIC_FUNCS, (list(eval_res.means().values()), list(eval_res.stddevs().values()))
+        
+    return loss_noise_dic
