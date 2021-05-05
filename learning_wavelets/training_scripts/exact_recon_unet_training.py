2b7374927d7fa5256d00c177015b3038b18da819
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-05-05 11:38:27 +0200

    Now the model assignment is a multi-line statement

--- learning_wavelets/training_scripts/exact_recon_unet_training.py
+++ learning_wavelets/training_scripts/exact_recon_unet_training.py
@@ -55,7 +55,12 @@ def train_unet(noise_std_train=(0, 55), noise_std_val=30, n_samples=None, source
     # run distributed 
     mirrored_strategy = tf.distribute.MirroredStrategy()
     with mirrored_strategy.scope():
-        model=ExactReconUnet(n_output_channels=1, kernel_size=3, layers_n_channels=[base_n_filters*2**j for j in range(0,n_layers)], non_linearity='relu')
+        model = ExactReconUnet(
+            n_output_channels=1, 
+            kernel_size=3, 
+            layers_n_channels=[base_n_filters*2**j for j in range(0, n_layers)], 
+            non_linearity='relu'
+        )
         model.compile(optimizer=tfa.optimizers.RectifiedAdam(), loss='mse')
     
 
