e79d3afe7def7c92c51e8ca0af2f4f717868cf9a
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-05-23 23:31:15 +0200

    Added residual param

--- learning_wavelets/models/exact_recon_unet.py
+++ learning_wavelets/models/exact_recon_unet.py
@@ -84,6 +84,7 @@ class ExactReconUnet(Model):
             non_linearity='relu',
             bn=False,
             exact_recon=False,
+            residual=False,
             **kwargs,
         ):
         super().__init__(**kwargs)
@@ -95,6 +96,7 @@ class ExactReconUnet(Model):
         self.non_linearity = non_linearity
         self.bn = bn
         self.exact_recon = exact_recon
+        self.residual = residual
         self.down_convs = [
             ConvBlock(
                 n_filters=n_channels,
@@ -140,7 +142,6 @@ class ExactReconUnet(Model):
         noisy_image = inputs[0]
         noise_std = inputs[1]
         outputs = noisy_image
-        outputs, h, w = pad_power_of_two(outputs, self.n_layers)
         for conv in self.down_convs:
             outputs = conv(outputs)
             scales.append(outputs)
@@ -152,44 +153,10 @@ class ExactReconUnet(Model):
             outputs = conv(outputs)
         outputs = self.final_conv(outputs)
         noise_std = tf.reshape(noise_std, shape=[tf.shape(noise_std)[0], 1, 1, 1])
-        outputs = tf.image.resize_with_crop_or_pad(outputs, h, w)
         if self.exact_recon:
             outputs = noisy_image - noise_std * outputs
-        else:
+        elif self.residual:
             outputs = noisy_image - outputs
         return outputs
     
 
-def pad_power_of_two(x, n_layers):
-    """ If padding is needed, the new width/height size can be
-    obtained by adding the corresponding power of two and subtracting
-    the rest of the division between the original width/height and
-    that same power of two.
-    
-    Example #1:
-    w = 23
-    power_of_two = 32
-    
-    new_w = 32 - 23%32 + 23
-    new_w = 32
-    
-    
-    Example #2:
-    w = 77
-    power_of_two = 32
-    
-    new_w = 32 - 77%32 + 77
-    new_w = 32 - 13 + 77
-    new_w = 96
-    """
-    
-    power_of_two = 2**n_layers
-
-    h = tf.shape(x)[1]
-    w = tf.shape(x)[2]
-
-    new_w = tf.cast(power_of_two*tf.math.ceil(w/power_of_two), tf.int32)
-    new_h = tf.cast(power_of_two*tf.math.ceil(h/power_of_two), tf.int32)
-
-    image_resized = tf.image.resize_with_crop_or_pad(x, new_h, new_w)        
-    return image_resized, h, w
