19b021a9a011a0c0be0f1668fe4ebe712301bb4f
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-07-23 11:28:21 +0200

    Removed the part that tests exact recon. No sense

--- learning_wavelets/tests/models/learnlet_model_test.py
+++ learning_wavelets/tests/models/learnlet_model_test.py
@@ -40,11 +40,3 @@ def test_fit(learnlet_kwargs):
     )
     K.clear_session()
 
-def test_exact_reconstruction():
-    model = Learnlet(**learnlet_test_cases[-1])
-    model.build([(None, 32, 32, 1), (None, 1)])
-    image = tf.random.uniform((1, 32, 32, 1), maxval=1, seed=0)
-    # we use a noise of level 0 to simulate no thresholding
-    res_image = model([image, tf.zeros(1, 1)])
-    res_psnr = tf.image.psnr(image, res_image, 1.).numpy()
-    assert res_psnr > 100
