cb47453df96786d3671171626d72ad3ff39c62b0
zaccharieramzi <email@example.com>
2022-03-22 14:58:34 +0100

    implemented bias free unet

--- learning_wavelets/models/exact_recon_unet.py
+++ learning_wavelets/models/exact_recon_unet.py
@@ -4,17 +4,27 @@ from tensorflow.keras.models import Model
 
 
 class Conv(Layer):
-    def __init__(self, n_filters, kernel_size=3, non_linearity='relu', bn=False, **kwargs):
+    def __init__(
+        self,
+        n_filters,
+        kernel_size=3,
+        non_linearity='relu',
+        bn=False,
+        use_bias=True,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
         self.n_filters = n_filters
         self.kernel_size = kernel_size
         self.non_linearity = non_linearity
         self.bn = bn
+        self.use_bias = use_bias
         self.conv = Conv2D(
             filters=self.n_filters,
             kernel_size=self.kernel_size,
             padding='same',
             activation=None,
+            use_bias=self.use_bias and not self.bn,
         )
         if self.non_linearity == 'lrelu':
             self.act = LeakyReLU(0.1)
@@ -22,8 +32,8 @@ class Conv(Layer):
             self.act = PReLU(shared_axes=[1, 2])
         else:
             self.act = Activation(self.non_linearity)
-        
-        self.bnorm = BatchNormalization()
+
+        self.bnorm = BatchNormalization(center=self.use_bias)
 
     def call(self, inputs):
         outputs = self.conv(inputs)
@@ -33,19 +43,30 @@ class Conv(Layer):
         return outputs
 
 class ConvBlock(Layer):
-    def __init__(self, n_filters, kernel_size=3, non_linearity='relu', bn=False, n_non_lins=2, **kwargs):
+    def __init__(
+        self,
+        n_filters,
+        kernel_size=3,
+        non_linearity='relu',
+        bn=False,
+        use_bias=True,
+        n_non_lins=2,
+        **kwargs,
+    ):
         super().__init__(**kwargs)
         self.n_filters = n_filters
         self.kernel_size = kernel_size
         self.non_linearity = non_linearity
         self.n_non_lins = n_non_lins
         self.bn = bn
+        self.use_bias = use_bias
         self.convs = [
             Conv(
                 n_filters=self.n_filters,
                 kernel_size=self.kernel_size,
                 non_linearity=self.non_linearity,
                 bn=self.bn,
+                use_bias=self.use_bias,
             ) for _ in range(self.n_non_lins)
         ]
 
@@ -56,15 +77,17 @@ class ConvBlock(Layer):
         return outputs
 
 class UpConv(Layer):
-    def __init__(self, n_filters, kernel_size=3, **kwargs):
+    def __init__(self, n_filters, kernel_size=3, use_bias=True, **kwargs):
         super().__init__(**kwargs)
         self.n_filters = n_filters
         self.kernel_size = kernel_size
+        self.use_bias = use_bias
         self.conv = Conv2D(
             filters=self.n_filters,
             kernel_size=self.kernel_size,
             padding='same',
             activation=None,
+            use_bias=self.use_bias,
         )
         self.up = UpSampling2D(size=(2, 2))
 
@@ -83,6 +106,7 @@ class ExactReconUnet(Model):
             layers_n_non_lins=2,
             non_linearity='relu',
             bn=False,
+            use_bias=True,
             exact_recon=False,
             residual=False,
             **kwargs,
@@ -95,6 +119,7 @@ class ExactReconUnet(Model):
         self.layers_n_non_lins = layers_n_non_lins
         self.non_linearity = non_linearity
         self.bn = bn
+        self.use_bias = use_bias
         self.exact_recon = exact_recon
         self.residual = residual
         self.down_convs = [
@@ -104,6 +129,7 @@ class ExactReconUnet(Model):
                 non_linearity=self.non_linearity,
                 n_non_lins=self.layers_n_non_lins,
                 bn=self.bn,
+                use_bias=self.use_bias,
             ) for n_channels in self.layers_n_channels[:-1]
         ]
         self.down = MaxPooling2D(pool_size=(2, 2), padding='same')
@@ -113,6 +139,7 @@ class ExactReconUnet(Model):
             non_linearity=self.non_linearity,
             n_non_lins=self.layers_n_non_lins,
             bn=self.bn,
+            use_bias=self.use_bias,
         )
         self.up_convs = [
             ConvBlock(
@@ -121,12 +148,14 @@ class ExactReconUnet(Model):
                 non_linearity=self.non_linearity,
                 n_non_lins=self.layers_n_non_lins,
                 bn=self.bn,
+                use_bias=self.use_bias,
             ) for n_channels in self.layers_n_channels[:-1]
         ]
         self.ups = [
             UpConv(
                 n_filters=n_channels,
                 kernel_size=self.kernel_size,
+                use_bias=self.use_bias,
             ) for n_channels in self.layers_n_channels[:-1]
         ]
         self.final_conv = Conv2D(
@@ -134,9 +163,10 @@ class ExactReconUnet(Model):
             kernel_size=1,
             padding='same',
             activation=None,
+            use_bias=self.use_bias,
         )
 
-    
+
     def call(self, inputs):
         scales = []
         noisy_image = inputs[0]
@@ -158,5 +188,3 @@ class ExactReconUnet(Model):
         elif self.residual:
             outputs = noisy_image - outputs
         return outputs
-    
-
