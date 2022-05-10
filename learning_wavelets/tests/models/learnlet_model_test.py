0150bfafb208079e9c44d8fc916df154a4bf1303
zaccharieramzi <zaccharie.ramzi@gmail.com>
2022-05-10 14:37:48 +0200

    added an equivariance test to the learnlet tests

--- learning_wavelets/tests/models/learnlet_model_test.py
+++ learning_wavelets/tests/models/learnlet_model_test.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pytest
 import tensorflow as tf
 import tensorflow.keras.backend as K
@@ -48,3 +49,14 @@ def test_exact_reconstruction():
     res_image = model([image, tf.zeros(1, 1)])
     res_psnr = tf.image.psnr(image, res_image, 1.).numpy()
     assert res_psnr > 100
+
+
+def test_equivariance():
+    model = Learnlet(**learnlet_test_cases[-1])  # exact reco
+    model.build([(None, 32, 32, 1), (None, 1)])
+    image = tf.random.uniform((1, 32, 32, 1), maxval=1, seed=0)
+    res_image = model([image, tf.ones(1, 1)])
+    scale_factor = 2
+    image_scaled = image * scale_factor
+    res_image_scaled = model([image_scaled, scale_factor * tf.ones(1, 1)])
+    np.testing.assert_allclose(res_image, res_image_scaled, atol=1e-5, rtol=0)
